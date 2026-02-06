8ab3ea4eaba6e1cfe8facf5f764020a42b40b90f
Ranjitha <ranjithaswamygowda2004@gmail.com>
2026-02-06 22:37:07 +0530

    Update frontend API URL

--- frontend/bug-tracker-frontend/src/api/axios.js
+++ frontend/bug-tracker-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://bug-tracker-mern-c2n5.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
